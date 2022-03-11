276e6d11205eefed529d14b1481208ea20ae19ed
Damien Fung <damienfung616@gmail.com>
2022-03-10 22:22:34 -0800

    Edited column parameter type

--- tests/test-filter.py
+++ tests/test-filter.py
@@ -6,5 +6,5 @@ df = pd.DataFrame({'a': ["Foo", "Bar"], 'b' : [1, 2]})
 expected_a = pd.DataFrame({'a': ["Foo"], 'b' : [1]})
 expected_b = pd.DataFrame({'a': ["Bar"], 'b' : [2]})
 
-assert_frame_equal(expected_a, filter(df, a, "Foo"))
-assert_frame_equal(expected_b, filter(df, b, 2))
+assert_frame_equal(expected_a, filter(df, "a", "Foo"))
+assert_frame_equal(expected_b, filter(df, "b", 2))
