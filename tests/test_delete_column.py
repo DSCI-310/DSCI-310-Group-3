5f4d9b438bf22d68b6d5beb0c6656a4ef6cdcc2a
Ryan Lazenby <ryanlazenby77@gmail.com>
2022-03-11 11:19:59 -0800

    fixed tests v2

--- tests/test_delete_column.py
+++ tests/test_delete_column.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sys
 sys.path.append(".")
-from Python import remove_column as rc
+from Python.remove_column import remove_column as rc
 
 # assign data of lists.  
 data = {'Name': ['Tom', 'Joseph', 'Krish', 'John'], 'Age': [20, 21, 19, 18]}
@@ -15,6 +15,6 @@ df = pd.DataFrame(data)
 expected_a = pd.DataFrame(data_a)
 expected_b = pd.DataFrame(data_b)
 
-assert data_a.equals(data.rc.remove_column(data, 'Age'))
-assert data_b.equals(data.rc.remove_column(data, 'Name'))
+assert expected_a.equals(rc(df, 'Age'))
+assert expected_b.equals(rc(df, 'Name'))
 
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -4,9 +4,9 @@ import numpy as np
 import sys
 
 sys.path.append(".")
-from Python import filter as f
+from Python.filter import filter as f
 
 df = pd.DataFrame({'a': ["Foo", "Bar"], 'b' : [1, 2]})
 expected = pd.DataFrame({'a': ["Foo"], 'b' : [1]})
-assert expected.equals(f.filter(df, "a", "Foo"))
+assert expected.equals(f(df, "a", "Foo"))
 
