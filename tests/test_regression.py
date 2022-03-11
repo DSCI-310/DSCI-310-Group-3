620d1a5f9c2022791f4268730be0ca8e451b249c
Ryan Lazenby <ryanlazenby77@gmail.com>
2022-03-11 11:06:30 -0800

    import fixes

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -3,11 +3,11 @@ import numpy as np
 import pandas as pd
 import sys
 sys.path.append(".")
-from Python import regression as r
+from Python.regression import regression
 
 x = [1,2]
 y = [2,4]
-results = pd.DataFrame(r.regression(x,y))
+results = pd.DataFrame(regression(x,y))
 
 # note: regression gives the following values, not exactly '2' and '0'
 expected_m = 1.9999999999999991
