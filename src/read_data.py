57ecf6ecd13242c1506a58365d2fc3d4cd7b3c2a
Alexander Khadra <alex.khadra@outlook.com>
2022-03-30 22:27:52 -0700

    Added read script
    This may contain a few errors and needs documentation

---
+++ src/read_data.py
@@ -0,0 +1,65 @@
+# add documentation 
+
+import pandas as pd
+from src.download_data import path
+from src.remove_column import remove_column
+
+    
+# load data, specifying there are no column names included in a header
+#data = pd.read_csv(path, header=None)   ## Path should be the output of download_data script
+
+def read_clean_data(data_path): #param should be from downlaod_data script
+    data_path = path #should this be added?
+    raw_data = pd.read_csv(data_path, header=None)
+    names = ['age',
+        'workclass',
+        'fnlwgt',
+        'education',
+        'education-num',
+        'marital-status',
+        'occupation',
+        'relationship',
+        'race',
+        'sex',
+        'capital-gain',
+        'capital-loss',
+        'hours-per-week',
+        'native-country',
+        'income']
+    raw_data.columns = names
+    raw_data = remove_column(raw_data, "race")
+    raw_data = remove_column(raw_data, "sex")
+    return raw_data.head()
+
+
+
+
+
+### Old version:
+# prepare column names
+#names = ['age',
+#        'workclass',
+#        'fnlwgt',
+#        'education',
+#        'education-num',
+#        'marital-status',
+#        'occupation',
+#        'relationship',
+#        'race',
+#        'sex',
+#        'capital-gain',
+#        'capital-loss',
+#        'hours-per-week',
+#        'native-country',
+#        'income']
+
+# assign column names
+#data.columns = names
+
+# drop some unused columns
+#data = remove_column(data, "race")
+#data = remove_column(data, "sex")
+
+# view examples in dataset
+#data.head()
+
