bcb3eeed710fc5b4fe32eb4229ae868db4ae595f
Ryan Lazenby <ryanlazenby77@gmail.com>
2022-04-08 00:53:26 -0700

    Update test_regression.py

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -5,18 +5,21 @@ import sys
 sys.path.append(".")
 from src.regression import regression
 
+# setup sample data
 x = [1,2]
 y = [2,4]
+# run regression, round to avoid OS-based discrepancies, and wrap in DataFrame
 results = pd.DataFrame(regression(x,y)).round(decimals=0)
 
-# note: regression gives the following values, not exactly '2' and '0'
+# compare regression with expected values, also in DataFrame for easy comparison
 expected_m = 2.0
 expected_b = 0.0
 expected = pd.DataFrame([expected_m, expected_b])
 
-assert expected.equals(results)
+# test that we get the expected values
+assert expected.equals(results), "Regression function did not return correct results"
 
-
+# a simple test to ensure we cannot run regression on all zeros
 def testLinAlgError():
     with pytest.raises(Exception):
         results = regression([0,0],[0,0])
